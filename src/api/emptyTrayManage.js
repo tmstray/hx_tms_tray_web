0b8ca6565ec061de5ee6ae8381f3d54cb0727890
huzhongwen <huzhongwen-hz@163.com>
2019-12-13 11:32:58 +0800

    修改查询条件

--- src/api/emptyTrayManage.js
+++ src/api/emptyTrayManage.js
@@ -2,17 +2,23 @@ import http from '@/config/httpConfig.js'
 const urlLists = {
     getStockLists: '/trayInfo/emptyTrayList',
     scrapOrLossTrayInfo:'/trayInfo/scrapOrLossTrayInfo',
-    forzenRow:'/trayInfo/freeZeTrayInfo'
+    forzenRow:'/trayInfo/freeZeTrayInfo',
+    exprotTemplate:'/trayInfo/importTemplate'
 }
 export const getStockLists = (params) => {
     return http.post(urlLists.getStockLists, params)
 }
 
-export const  scrapOrLossTrayInfo= (params) => {
+export const  scrapOrLossTrayInfo = (params) => {
     return http.post(urlLists.scrapOrLossTrayInfo, params)
 }
 
-export const  forzenRow= (params) => {
+export const  forzenRow = (params) => {
     return http.post(urlLists.forzenRow, params)
 }
+
+export const  exprotTemplate = (params) => {
+    return http.get(urlLists.exprotTemplate, params)
+}
+
 
