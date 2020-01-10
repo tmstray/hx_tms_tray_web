2979bbe1ba063622d8166c6667d7221e5d51922e
zwm <747309718@qq.com>
2020-01-10 15:54:02 +0800

    菜单

--- src/pages/role/index.vue
+++ src/pages/role/index.vue
@@ -283,8 +283,6 @@ export default {
             for(let i=0;i<this.$refs.tree.getCheckedNodes(false,true).length;i++){
                  this.roleData.menuIds.push(this.$refs.tree.getCheckedNodes(false,true)[i])
             }
-            console.log(this.roleData.menuIds);
-            return ;
             if(this.type=='add'){
                 addRole(this.roleData).then(res=>{
                     if(res.status=='200'){
